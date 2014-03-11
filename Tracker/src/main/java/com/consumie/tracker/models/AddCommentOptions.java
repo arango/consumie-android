acee1c2f909d4961b3c971bd658f4730cfe4820f
Padgett Arango <padgett@developmentnow.com>
2014-03-11 14:02:25 -0700

    no message

---
+++ Tracker/src/main/java/com/consumie/tracker/models/AddCommentOptions.java
@@ -0,0 +1,10 @@
+package com.consumie.tracker.models;
+
+/**
+ * Created by Arango on 2/15/14.
+ */
+public class AddCommentOptions {
+    public String notes;
+    public String userKey;
+    public Integer consumptionID;
+}
---
+++ Tracker/src/main/java/com/consumie/tracker/util/CommentEditText.java
@@ -0,0 +1,39 @@
+package com.consumie.tracker.util;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.EditText;
+
+/**
+ * Created by Arango on 2/16/14.
+ */
+public class CommentEditText extends EditText {
+    public CommentEditText(Context context) {
+        super(context);
+    }
+
+    public CommentEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CommentEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+    @Override
+    public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
+        InputConnection connection = super.onCreateInputConnection(outAttrs);
+        int imeActions = outAttrs.imeOptions&EditorInfo.IME_MASK_ACTION;
+        if ((imeActions&EditorInfo.IME_ACTION_DONE) != 0) {
+            // clear the existing action
+            outAttrs.imeOptions ^= imeActions;
+            // set the DONE action
+            outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
+        }
+        if ((outAttrs.imeOptions&EditorInfo.IME_FLAG_NO_ENTER_ACTION) != 0) {
+            outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
+        }
+        return connection;
+    }
+}
